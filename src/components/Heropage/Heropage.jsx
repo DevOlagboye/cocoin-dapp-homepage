875c71fdb8eec6f3007e3448c8a12ecde4cf020e
Olagboye Seyi <akingbolaola@gmail.com>
2022-11-17 16:59:18 +0100

    Job done for today

--- src/components/Heropage/Heropage.jsx
+++ src/components/Heropage/Heropage.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import './Heropage.css'
+import logo from "../../assets/images/logo.png"
+import line from "../../assets/images/Line.png"
 
 const Heropage = () => {
   return (
-    <div>Heropage</div>
+    <div className='hero-container'>
+        <div className='logo-menu'>
+        <div className='logo'>
+            <img src={logo} alt="Company logo" />
+        </div>
+        <div className='menu-list'>
+            <ul>
+                <li>Licence</li>
+                <li>About us</li>
+                <li>How to start</li>
+                <button>Join us</button>
+            </ul>
+        </div>
+        </div>
+        <div className='line'>
+        <img src={line} alt="" srcset="" />
+        </div>
+        <h2 className='hero-text'>The World’s Fastest Growing Crypto Community</h2>
+        <p>A highly-curated platform for creating, collecting and trading unique<br/> NFTs. Trade with confidence on the world’s fastest and most secure crypto <br/>exchange</p>
+    </div>
   )
 }
 
