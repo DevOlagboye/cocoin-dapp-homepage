d8157031fd10f27a4a7864d61bb2d75f2f1095d0
Olagboye Seyi <akingbolaola@gmail.com>
2022-11-18 14:09:00 +0100

    About component up next

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Heropage from './components/Heropage/Heropage';
+import About from './components/Heropage/About/About';
 function App() {
   return (
     <div>
       <Heropage/>
+      <About/>
     </div>
   );
 }
