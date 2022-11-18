1cc5c3306e6c1eb3bf58788c1e031ef6170f1011
Olagboye Seyi <akingbolaola@gmail.com>
2022-11-18 14:06:21 +0100

    Homepage component almost done

--- src/components/Heropage/Heropage.jsx
+++ src/components/Heropage/Heropage.jsx
@@ -24,6 +24,13 @@ const Heropage = () => {
         </div>
         <h2 className='hero-text'>The World’s Fastest Growing Crypto Community</h2>
         <p>A highly-curated platform for creating, collecting and trading unique<br/> NFTs. Trade with confidence on the world’s fastest and most secure <br/> crypto exchange</p>
+        <div>
+            <form className='hero-form'>
+            <input type="email" placeholder="Your Email Address" className='hero-input'/>
+            <button className='hero-button'>Join our Community</button>
+            </form>
+            
+        </div>
     </div>
   )
 }
