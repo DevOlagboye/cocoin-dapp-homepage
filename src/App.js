d9c9676776d250f4459ae9e2be2c08d99653d9f7
Olagboye Seyi <akingbolaola@gmail.com>
2022-11-21 14:35:16 +0100

    Added Direction component as well as Started the design of the component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Heropage from './components/Heropage/Heropage';
 import About from './components/About/About';
+import Direction from './components/Direction/Direction';
 function App() {
   return (
     <div>
       <Heropage/>
       <About/>
+      <Direction/>
     </div>
   );
 }
---
+++ src/components/Direction/Direction.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Direction.css"
+
+const Direction = () => {
+  return (
+    <div className='direction-container'>
+        <div className='direction-text'>
+            <h3>Our directions and advantages</h3>
+            <p>Integrate once and never worry about scaling again. We ensure composability between<br/> ecosystem projects by maintaining a single global state as the network scales.</p>
+        </div>
+    </div>
+  )
+}
+
+export default Direction
